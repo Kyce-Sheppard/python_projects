412cccdf3977ab02513101d969fde6b6c1dc1b8a
Kyce <135944962+Kyce-Sheppard@users.noreply.github.com>
2023-08-01 13:31:41 -0500

    Add files via upload
    This is a program that simply keeps a list of cocktail recipes in a .txt file. Might build this into a full application or mobile app or something. Just wanted to practice with some basic loops and inputs.

---
+++ drink_manager.py
@@ -0,0 +1,35 @@
+# A project to keep links for drink recipes. I just want to get it working and then work on customization
+
+def add_drink():
+    drink_name = input("Recipe name: ")
+    drink_type = input("What is the drink base? ")
+    drink_link = input("Paste the link: ")
+
+    with open('drink_recipes.txt', 'a') as f:
+        f.write(f"{drink_name} | {drink_type} | {drink_link}\n")
+
+def view_drink():
+    drink_recipes = []
+    with open('drink_recipes.txt', 'r') as f:
+        for line in f.readlines():
+            drink_name, drink_type, drink_link = line.strip().split('|')
+            print(f"Drink: {drink_name}| Base spirit: {drink_type}| Link: {drink_link}")
+            drink_recipes.append({
+                'drink_name': drink_name,
+                'drink_type': drink_type,
+                'drink_link': drink_link
+            })
+
+while True:
+    mode = input(
+        "Would you like to add a new drink recipe or view the list (view, add), press q to quit. "
+        ).lower()
+    if mode == "q":
+        break
+    if mode == "view":
+        view_drink()
+    elif mode == "add":
+        add_drink()
+    else:
+        print("Invalid mode.")
+        continue
